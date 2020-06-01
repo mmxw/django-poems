9fd9e5025871fa58fd7323719c2068491078a24e
cara <xw33@outlook.com>
2020-06-01 15:39:41 +0100

    set up templates

--- project/poems/views.py
+++ project/poems/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Collection
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("live a poetic life")
+    collections = Collection.objects.all()
+    return render(request, 'home.html', {'collections': collections} )
 
